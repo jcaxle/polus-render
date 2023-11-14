11cecd2295a41e714537791d7d55abc4703748eb
Jeff Chen <jeff.chen@AXLE-MacBookPro-C02XN0G4KQCJ.local>
2023-11-14 11:41:13 -0500

    Add nb_render() func, needs testing

--- src/polus.py
+++ src/polus.py
@@ -27,6 +27,51 @@ def run_local_render(port:int)->None:
     Thread(target=host_application, args=(Path(pkg_resources.resource_filename(__name__, "apps/render-ui")),port,)).start()
 
 
+def nb_render(nbhub_url:ParseResult,image_location:Union[ParseResult, PurePath] = "", microjson_overlay_location:Union[ParseResult, PurePath] = "", width:int=960, height:int=500, image_port:int=0, \
+           microjson_overlay_port:int=0, use_local_render:bool=True, render_url:str = "https://render.ci.ncats.io/")->str:
+    """
+    Variant of render() used for nbhub. Read render() for usage information
+
+    Param:
+        nbhub_url: URL used used for jupyterhub. Contains '/lab/' in its uri
+        image_location(ParseResult|Purepath): Acquired from urllib.parse.ParseResult or Path, renders url in render.
+                            If not specified, renders default render url.
+        microjson_overlay_location(ParseResult|Purepath): Acquired from urllib.parse.ParseResult or Path, renders url in render.
+                            If not specified, renders default render url
+        width (int): width of render to be displayed, default is 960
+        height (int): height of render to be displayed, default is 500
+        run_local_render (bool): True to run local build of render with 1st available port, False to use render_url (default is True)
+        render_url (str): URL which refers to Polus Render. Used when run_local_render is False. (default is https://render.ci.ncats.io/)
+    Pre: zarr_port and json_port selected (if used) is not in use IF path given is Purepath
+    Returns: Render URL
+    """
+    assert(nbhub_url)
+    base_nbhub = nbhub_url.geturl().rpartition("lab")[0]
+    # Extract url from local file path if provided. ?imageUrl is required scheme for render
+    if isinstance(image_location, PurePath):
+        image_location = "?imageUrl=" + base_nbhub + "serve/file/" + image_location
+
+    # Otherwise, extract url from user provided url if provided
+    elif isinstance(image_location, ParseResult):
+        image_location = "?imageUrl=" + image_location.geturl() # Can be manually rebuilt to check if a valid format url is sent
+    
+    # Do the same but for JSON
+    if isinstance(microjson_overlay_location, PurePath):
+        microjson_overlay_location = "&overlayUrl=" + base_nbhub + "serve/file/" + microjson_overlay_location
+
+    elif isinstance(microjson_overlay_location, ParseResult):
+        microjson_overlay_location = "&overlayUrl=" + microjson_overlay_location.geturl()    
+
+    # Local render
+    if use_local_render:
+        render_ui = f"{base_nbhub}static/serve/render-ui/index.html"
+    
+    # Display render
+    display(IFrame(src=(f"{render_ui}{image_location}{microjson_overlay_location}")
+                                                        , width=width, height=height))
+    
+    return f"{render_ui}{image_location}{microjson_overlay_location}"
+
 def render(image_location:Union[ParseResult, PurePath] = "", microjson_overlay_location:Union[ParseResult, PurePath] = "", width:int=960, height:int=500, image_port:int=0, \
            microjson_overlay_port:int=0, use_local_render:bool=True, render_url:str = "https://render.ci.ncats.io/")->str:
     """
@@ -42,13 +87,12 @@ def render(image_location:Union[ParseResult, PurePath] = "", microjson_overlay_l
         height (int): height of render to be displayed, default is 500
         image_port (int): Port to run local zarr server on if used (default is 0 which is the 1st available port).
         microjson_overlay_port (int): Port to run local json server on if used (default is 0 which is the 1st available port).
-        run_local_render (bool): True to run local build of render with 1st available port, False to use render_url (default is True)
+        use_local_render (bool): True to run local build of render with 1st available port, False to use render_url (default is True)
         render_url (str): URL which refers to Polus Render. Used when run_local_render is False. (default is https://render.ci.ncats.io/)
     Pre: zarr_port and json_port selected (if used) is not in use IF path given is Purepath
     Returns: Render URL
         
     """
-
     # Extract url from local file path if provided. ?imageUrl is required scheme for render
     if isinstance(image_location, PurePath):
 
