c91286cd50f2d8bf587db021c4b3ce7536b888d0
Jeff Chen <jeff.chen@AXLE-MacBookPro-C02XN0G4KQCJ.local>
2023-11-22 09:42:40 -0500

    Removed online Render build functionality

--- polus/polus_render.py
+++ polus/polus_render.py
@@ -6,14 +6,13 @@ from typing import Union
 
 
 
-def render(nbhub_url:ParseResult, nb_root:PurePath = Path("/home/jovyan/"), image_location:Union[ParseResult, PurePath] = "", microjson_overlay_location:Union[ParseResult, PurePath] = "", width:int=960, height:int=500, \
-            use_local_render:bool=True, render_url:str = "https://render.ci.ncats.io/")->str:
+def render(nbhub_url:ParseResult, nb_root:PurePath = Path("/home/jovyan/"), image_location:Union[ParseResult, PurePath] = "", microjson_overlay_location:Union[ParseResult, PurePath] = "", width:int=960, height:int=500)->str:
     """
     Variant of render() used for remote jupyter notebooks. Read render() for usage information
 
     Param:
-        nbhub_url: URL used used for jupyterhub. Contains '/lab/' in its uri
-        nb_root: Root path used to search files in. Default is '/home/jovyan/' which works for notebooks hub. Can be set to empty path 
+        nbhub_url (ParseResult): URL used used for jupyterhub. Contains '/lab/' in its uri
+        nb_root (ParseResult): Root path used to search files in. Default is '/home/jovyan/' which works for notebooks hub. Can be set to empty path 
                 if absolute paths will be used for images and json files.
         image_location(ParseResult|Purepath): Acquired from urllib.parse.ParseResult or Path, renders url in render.
                             If not specified, renders default render url.
@@ -21,9 +20,6 @@ def render(nbhub_url:ParseResult, nb_root:PurePath = Path("/home/jovyan/"), imag
                             If not specified, renders default render url
         width (int): width of render to be displayed, default is 960
         height (int): height of render to be displayed, default is 500
-        run_local_render (bool): True to run local build of render with 1st available port, False to use render_url (default is True)
-        render_url (str): URL which refers to Polus Render. Used when run_local_render is False. (default is https://render.ci.ncats.io/)
-    Pre: zarr_port and json_port selected (if used) is not in use IF path given is Purepath
     Returns: Render URL
     """
     assert(nbhub_url)
@@ -44,8 +40,7 @@ def render(nbhub_url:ParseResult, nb_root:PurePath = Path("/home/jovyan/"), imag
         microjson_overlay_location = "&overlayUrl=" + microjson_overlay_location.geturl()    
 
     # Local render
-    if use_local_render:
-        render_url = f"{base_nbhub}static/serve/render-ui/index.html"
+    render_url = f"{base_nbhub}static/serve/render-ui/index.html"
     
     # Display render
     display(IFrame(src=(f"{render_url}{image_location}{microjson_overlay_location}")
